25d7c94b3f0eb1a35696a024f87c23b83b919f62
giahienpham <giahien14604@gmail.com>
2024-08-17 01:18:41 -0400

    buiding cart page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
+import { CartComponent } from "./pages/cart/cart.component";
 
 const routes: Routes = [
   {
@@ -8,10 +9,14 @@ const routes: Routes = [
     component: HomeComponent,
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  }
+    path: "cart",
+    component: CartComponent,
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
 ];
 
 @NgModule({
